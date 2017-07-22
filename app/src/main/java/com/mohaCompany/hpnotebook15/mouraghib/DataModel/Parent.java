b4cbd99785deca1564db16e71b347a7218fbcbfa
Mohamed Hamed <hamedm@myumanitoba.ca>
2017-07-21 21:34:53 -0500

    update Parent class

--- app/src/main/java/com/mohaCompany/hpnotebook15/mouraghib/DataModel/Parent.java
+++ app/src/main/java/com/mohaCompany/hpnotebook15/mouraghib/DataModel/Parent.java
@@ -1,8 +1,46 @@
 package com.mohaCompany.hpnotebook15.mouraghib.DataModel;
 
+import java.util.ArrayList;
+
 /**
  * Created by HPnotebook15 on 2017-07-21.
  */
 
 public class Parent {
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private ArrayList<Student> enfants;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public ArrayList<Student> getEnfants() {
+        return enfants;
+    }
+
+    public void setEnfants(ArrayList<Student> enfants) {
+        this.enfants = enfants;
+    }
 }
